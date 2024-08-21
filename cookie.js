2d0c71bd25e1466f021a8623dcc13f3771d3b02e
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-21 10:11:20 +0600

    retrieve local storage anda display them

--- cookie.js
+++ cookie.js
@@ -1,7 +1,7 @@
 const getCookie = name => {
     const cookie = document.cookie;
     const allCookie = cookie.split('; ');
-    const findCookie = allCookie.find(c => c.inclodes(name));
+    const findCookie = allCookie.find(c => c.includes(name));
     if (findCookie) {
         'country=US'
         const cookieNameValue = findCookie.split('=');
